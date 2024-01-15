c11b85c1aa5add734a44510202d64df6b93c0ffb
Ieva Swanson <ievaswanson@u.boisestate.edu>
2024-01-15 17:40:57 -0600

    Ch 8 more doodling around with chapter questions

--- hello.js
+++ hello.js
@@ -43,3 +43,65 @@ console.log(classes.length);
 
 teachers = ["Jones", "Willoughby", "Rhodes"];
 console.log(teachers.length);
+
+//questions about sort: checking my answer 
+// I think [23, 350, 47, 'Tech', 'coder'] is the correct answer
+
+let charles = ['coder', 'Tech', 47, 23, 350];
+charles.sort();
+console.log(charles);
+
+//Woot! got it!
+//question: Which statement converts the string 
+//str = 'LaunchCode students rock!' into the array 
+//['LaunchCode', 'students', 'rock!']?
+// I think str.split(" "); will split the string at each space
+
+let strOne = 'LaunchCode students rock!';
+let arrOne = [];
+arrOne = strOne.split(" ");
+console.log(arrOne);
+
+//woo hoo! vindicated!!
+//What is printed by the following program?
+//my answer before running program:
+//['chips', 'cucumbers', 'edamame']
+
+let groceryBag = ['bananas', 'apples', 'edamame', 'chips', 'cucumbers', 'milk', 'cheese'];
+let selectedItems = [];
+
+selectedItems = groceryBag.slice(2, 5).sort();
+console.log(selectedItems);
+
+//YAY!! I can read code I just can't write it YET
+//
+// Question: What are the two dimensional indexes for "Jones"?
+
+let school = [
+    ["science", "computer", "art"],
+    ["Jones", "Willoughby", "Rhodes"]
+];
+
+// My answer: the outer array index is 0, 1. 1 is the outer index for "Jones"
+// The inner array index is 0, 1, 2. 0 is the inner index for "Jones"
+
+//How would you add "dance" to the array at school[0]?
+
+//I guess this question is looking at the first inner array
+//this looks like a list of classes
+//we want to "push" another class into outer array 0, inner array 3.
+//multiArrayName[indexOfInnerArray].method();
+
+let danceClass = 'dance';
+school[0].push(danceClass);
+console.log(school[0]);
+
+//Whew that took some fiddling around
+
+//How would you add "Holmes" to the array at school[1]?
+
+let danceTeacher = 'Holmes';
+school[1].push(danceTeacher);
+console.log(school[1]);
+
+//Easier the second time!
